7d793b33e7df5f81fbe9ead7e51b58c44f852e10
unknown <alphagma@gmail.com>
2016-01-04 07:03:07 -0600

    Fixed level 2+ titles being mistaken as tags

--- post-processing/strip-tags.js
+++ post-processing/strip-tags.js
@@ -2,7 +2,7 @@
 
 module.exports = stripTags;
 
-var TAG_REGEX = /#\S+/g;
+var TAG_REGEX = /#[^\s#]+/g;
 
 function stripTags(content/*, options*/) {
 	console.log('Stripping tags from ' + content.split('\n')[0]);
